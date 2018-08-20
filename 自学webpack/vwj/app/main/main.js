1093362502e47694259f3619afb466166f851637
dearseven <dearseven@sina.com>
2018-08-20 17:10:47 +0800

    'bycyan'

--- 自学webpack/vwj/app/main/main.js
+++ 自学webpack/vwj/app/main/main.js
@@ -2,13 +2,36 @@ import Vue from 'vue'
 import './main.css'; //使用require导入css文件
 //引入一个js，反正这里写的也是源码路径，但是会按照webpack打包以后的路径引入,具体我们不管
 require('../../js/title_change.js')
-import  '../../css/global.css'
+import '../../css/global.css'
 
 import banner from "../widgets/banner.vue"
 
-new Vue({
+var bn = new Vue({
 	el: '#_banner',
 	components: {
 		banner
+	},
+	methods: {
+		/*
+		 * 一下两个方法其实是要调用banner.vue里面的方法，banner是this的第一个子元素所以~~要再调用一次
+		 */
+		changeObjects: function(k, v) {
+			//顺便说一下 可以通过这个banner的root元素的id来判断，这里打印出来的id就是banner.vue的根元素的id
+			console.log(this.$children[0].$el.id)
+			//这个改的是一个json
+			this.$children[0].changeObjects(k, v)
+		},
+		changeObject2: function(i, v) {
+			//这个改的是一个数组
+			this.$children[0].changeObject2(i, v)
+		}
 	}
 })
+let _f = () => {
+	bn.changeObjects('lastName', "setTimeout");
+	bn.changeObject2(0, "setTimeout");
+}
+window.setTimeout(
+	_f,
+	1000
+)
--- 自学webpack/vwj/app/widgets/banner.vue
+++ 自学webpack/vwj/app/widgets/banner.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
+	<div id="bannerref">
 		我用到了组合vue组件：第二个列表用了atag组件
 		<br/>
 		<ul id="v-for-object" class="banner-ul">
-			<li v-for="(value, key) in object" class="banner-li">
-				<a href="">{{ key }}={{ value }}</a>
+			<li v-for="(value, key) in objects" class="banner-li">
+				<a href="">{{ key }}_{{ value }}</a>
 			</li>
 		</ul>
 		<br/>
@@ -23,14 +23,15 @@
 	//至于这为什么要用这个路径，我是猜因为编译的时候实际上是用的入口地址在匹配的
 	//如果地址是main.js所以（所以所有入口需要是同层级的目录），所以atag的地址才是../widgets/atag.vue
 	import atag from "../widgets/atag.vue"
+	import Vue from 'vue'
 
 	export default {
 		created: function() {
-
+			this.changeObjects("firstName", "changeBycreated");
 		},
 		data() {
 			return {
-				object: {
+				objects: {
 					firstName: 'John',
 					lastName: 'Doe',
 					age: 30
@@ -44,6 +45,14 @@
 		},
 		components: {
 			atag
+		},
+		methods: {
+			changeObjects: function(k, v) {
+				Vue.set(this.objects, k, v)
+			},
+			changeObject2: function(i, v) {
+				Vue.set(this.object2, i, v)
+			}
 		}
 	}
 </script>
@@ -58,6 +67,6 @@
 	.li {
 		list-style-type: none;
 		float: left;
-		width: 208px;
+		width: 248px;
 	}
 </style>
