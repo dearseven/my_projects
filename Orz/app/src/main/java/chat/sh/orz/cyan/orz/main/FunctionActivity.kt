137cda351596ccbaaf9f1e16ee1be3ebea78780d
dearseven <dearseven@sina.com>
2018-09-19 12:07:20 +0800

    bycyan

--- Orz/app/src/main/java/chat/sh/orz/cyan/orz/main/FunctionActivity.kt
+++ Orz/app/src/main/java/chat/sh/orz/cyan/orz/main/FunctionActivity.kt
@@ -36,8 +36,10 @@ class FunctionActivity : AppCompatActivity() {
     var binding: ActivityFunctionBinding? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //获取绑定
         binding = DataBindingUtil.setContentView<ActivityFunctionBinding>(this, R.layout.activity_function)
         //setContentView(R.layout.activity_function)
+        //初始化一些数据
         funcList.add(FunctionViewModel("图片选取or拍照1", MainActivity::class.java))
         funcList.add(FunctionViewModel("图片选取or拍照2", MainActivity::class.java))
         funcList.add(FunctionViewModel("图片选取or拍照3", MainActivity::class.java))
@@ -50,25 +52,33 @@ class FunctionActivity : AppCompatActivity() {
         funcList.add(FunctionViewModel("图片选取or拍照10", MainActivity::class.java))
         funcList.add(FunctionViewModel("图片选取or拍照11", MainActivity::class.java))
 
-        //
+        //初始化adapter
         adapter = Adapter()
-        //***1***侧滑和拖动数据 这个不要了
-        //先实例化Callback
+
+        //***1***侧滑和拖动数据 这个没用了 因为我没有用sdk提供的swipe和拖拽
+        //先实例化Callback 这个没用了 因为我没有用sdk提供的swipe和拖拽
         var callback = SimpleItemTouchHelperCallback(adapter!!);
-        //用Callback构造ItemtouchHelper
+        //用Callback构造ItemtouchHelper 这个没用了 因为我没有用sdk提供的swipe和拖拽
         var touchHelper = ItemTouchHelper(callback);
         //调用ItemTouchHelper的attachToRecyclerView方法建立联系
+        //这个没用了 因为我没有用sdk提供的swipe和拖拽
         touchHelper.attachToRecyclerView(funRv);
-        //设置
-        funRv.setLayoutManager(LinearLayoutManager(this))
+        //***2***基本设置
+        funRv.layoutManager = LinearLayoutManager(this)
         funRv.addItemDecoration(MyItemDecoration())
+        //---这个adapter就有意思
+        // 实际上是上是装饰器模式,MyRecyclerView重写了原本的setAdapter
+        //可以追进去看,实际上是用另一个适配器(MyWrapAdapter)添加了header和footer
+        //然后内容部分会调用这个代码里的适配器来实现
         funRv.adapter = adapter!!
         //***2***下拉刷新和上来加载更多的回调
         funRv.myRecyclerViewListener = object : MyRecyclerViewListener {
+            //下拉刷新的回调,做一些事件和回收header
             override fun onRefresh() {
                 h.postDelayed({ funRv.setRefreshComplete() }, 1500)
             }
 
+            //加载更多的回调,做一些事件和回收footer
             override fun onLoadMore() {
                 h.postDelayed({
                     for (i in 0..4) {
@@ -83,6 +93,10 @@ class FunctionActivity : AppCompatActivity() {
     }
 
     inner class Adapter : CRecyclerListAdapter() {
+        /**
+         * 我在SimpleItemTouchHelperCallback的isItemViewSwipeEnabled禁止了swipe
+         */
+        @Deprecated("没用自带的")
         override fun onItemDissmiss(position: Int) {
 //            Log.d("ORZ", "11!!!onItemDissmiss###:"+funcList.size)
 //            //移除数据
@@ -94,8 +108,9 @@ class FunctionActivity : AppCompatActivity() {
         }
 
         /**
-         * 我再SimpleItemTouchHelperCallback的isLongPressDragEnabled禁止了拖动
+         * 我在SimpleItemTouchHelperCallback的isLongPressDragEnabled禁止了拖动
          */
+        @Deprecated("没用自带的")
         override fun onItemMove(fromPosition: Int, toPosition: Int) {
 //            //交换位置
 //            Collections.swap(funcList, fromPosition, toPosition);
@@ -115,19 +130,20 @@ class FunctionActivity : AppCompatActivity() {
             var bind: FunRecyclerItemLayoutBinding = DataBindingUtil.getBinding<FunRecyclerItemLayoutBinding>(holder.itemView)!!
             bind.funs = data;
             bind.executePendingBindings();
-            //
+            //---这个是点击事件
             holder.itemView.findViewById<TextView>(R.id.show).setOnClickListener(object : View.OnClickListener {
                 override fun onClick(view: View) {
                     Toast.makeText(this@FunctionActivity, "编号：$position", Toast.LENGTH_LONG).show()
                 }
             })
+            //---这个是滑动出现的删除按钮的点击事件
             holder.itemView.findViewById<TextView>(R.id.click).setOnClickListener(object : View.OnClickListener {
                 override fun onClick(p0: View?) {
                     Toast.makeText(this@FunctionActivity, "删除：$position", Toast.LENGTH_LONG).show()
                 }
 
             })
-            //恢复状态
+            //恢复滚动条的状态,在view重复使用的时候,就不会出现一开始删除区块就滑出来了
             holder.itemView.findViewById<MyRecyclerViewItem>(R.id.scroll_item).reset()
         }
 
--- Orz/app/src/main/java/chat/sh/orz/cyan/orz/main/MyRecyclerViewItem.java
+++ Orz/app/src/main/java/chat/sh/orz/cyan/orz/main/MyRecyclerViewItem.java
@@ -11,7 +11,7 @@ import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
 public class MyRecyclerViewItem extends HorizontalScrollView {
-    private boolean isLeft=true;//在左边
+    private boolean isLeft = true;//在左边
 
     public MyRecyclerViewItem(Context context) {
         super(context);
@@ -39,67 +39,60 @@ public class MyRecyclerViewItem extends HorizontalScrollView {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        Log.i(getClass().getSimpleName(), l+"-"+t+"-"+r+"-"+b);
+        Log.d("ORZ", l + "-" + t + "-" + r + "-" + b);
 
         //调整布局
-        LinearLayout linearLayout= (LinearLayout) getChildAt(0);
+        LinearLayout linearLayout = (LinearLayout) getChildAt(0);
         LinearLayout linearLayoutLeft = (LinearLayout) linearLayout.getChildAt(0);
         LinearLayout linearLayoutRight = (LinearLayout) linearLayout.getChildAt(1);
-        linearLayout.layout(linearLayout.getLeft(), linearLayout.getTop(), linearLayout.getLeft()+getMeasuredWidth()+ buttonWidth, linearLayout.getBottom());
-        linearLayoutLeft.layout(linearLayoutLeft.getLeft(), linearLayoutLeft.getTop(),linearLayoutLeft.getLeft()+getMeasuredWidth(), linearLayoutLeft.getBottom());
-        linearLayoutRight.layout(linearLayoutLeft.getLeft()+getMeasuredWidth(), linearLayoutLeft.getTop(), linearLayoutLeft.getLeft()+getMeasuredWidth()+ buttonWidth, linearLayoutLeft.getBottom());
+        linearLayout.layout(linearLayout.getLeft(), linearLayout.getTop(), linearLayout.getLeft() + getMeasuredWidth() + buttonWidth, linearLayout.getBottom());
+        linearLayoutLeft.layout(linearLayoutLeft.getLeft(), linearLayoutLeft.getTop(), linearLayoutLeft.getLeft() + getMeasuredWidth(), linearLayoutLeft.getBottom());
+        linearLayoutRight.layout(linearLayoutLeft.getLeft() + getMeasuredWidth(), linearLayoutLeft.getTop(), linearLayoutLeft.getLeft() + getMeasuredWidth() + buttonWidth, linearLayoutLeft.getBottom());
 
     }
 
 
     //恢复状态
-    public void reset(){
-        isLeft=true;
-        scrollTo(0,0);
+    public void reset() {
+        isLeft = true;
+        scrollTo(0, 0);
     }
 
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        if (ev.getAction()== MotionEvent.ACTION_DOWN)
-        {
-            Log.i(getClass().getSimpleName(), "down");
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.d("ORZ", "MyRecyclerViewItem::down");
             return true;
         }
-        if (ev.getAction()==MotionEvent.ACTION_CANCEL || ev.getAction()== MotionEvent.ACTION_UP)
-        {
-            Log.i(getClass().getSimpleName(), "up");
-            int range=70;
-            if (isLeft)
-            {
-                if (getScrollX()>range)
-                {
-                    isLeft=false;
+        if (ev.getAction() == MotionEvent.ACTION_CANCEL || ev.getAction() == MotionEvent.ACTION_UP) {
+            Log.d("ORZ", "MyRecyclerViewItem::up");
+            int range = 70;
+            if (isLeft) {
+                if (getScrollX() > range) {
+                    isLeft = false;
                     smoothScrollTo(buttonWidth, 0);
-                }else {
-                    smoothScrollTo(0,0);
+                } else {
+                    smoothScrollTo(0, 0);
                 }
-            }else {
-                if (getScrollX()< (buttonWidth-range))
-                {
-                    isLeft=true;
-                    smoothScrollTo(0,0);
-                }else {
+            } else {
+                if (getScrollX() < (buttonWidth - range)) {
+                    isLeft = true;
+                    smoothScrollTo(0, 0);
+                } else {
                     smoothScrollTo(buttonWidth, 0);
                 }
             }
             return true;
         }
-        Log.i(getClass().getSimpleName(), "end");
+        Log.d("ORZ", "MyRecyclerViewItem::getScrollX() ="+getScrollX() );
         return super.onTouchEvent(ev);
     }
 
 
-    private int dp2px(float dpValue){
+    private int dp2px(float dpValue) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, scale);
     }
-
-
 
 
 }
