0654f1bc949ae5fe40e2cc135681060cc7897e22
cyank1986 <54947314@qq.com>
2019-07-17 16:59:56 +0800

    bycyan

--- js/fixed-table-cyan/js/main.js
+++ js/fixed-table-cyan/js/main.js
@@ -1,19 +1,42 @@
 /**
- * Created by petter on 15/11/28.
+ * 找个其实是可滑动表格的css
  */
 
 var win = $(window),
-    scrollAreaEl = $('.t_r_content'),
-    leftFreezeEl = $('.t_l_freeze'),
-    leftTableEl = leftFreezeEl.find('table'),
-    rightTableEl = $('.t_r_t table');
+	scrollAreaEl = $('.t_r_content'),
+	leftFreezeEl = $('.t_l_freeze'),
+	leftTableEl = leftFreezeEl.find('table'),
+	rightTableEl = $('.t_r_t table');
 
-//动态计算容器最大高度
+//**cyan动态计算容器最大高度 这里有几个要注意的地方，
+//1 因为不好计算表格的高度，所以高度是总高度-其他区域的高度和=表格的显示框高度（表格可能大于这个值）
+//2 tableHeight = winHeight - contentHeight- 60;这个-60是内容格子的高度
 function adjustHeight() {
-    var winHeight = win.height(),
-        tableHeight = winHeight - 90;
-    leftFreezeEl.height(tableHeight);
-    scrollAreaEl.height(tableHeight);
+	var winHeight = win.height();
+		 //tableHeight = winHeight - 90;
+
+	var totalHeight = 0;
+	var contentHeight = 0;
+	var box = $("#area_1")[0]
+	contentHeight += parseFloat(window.getComputedStyle(box).height.replace("px", ""));
+	//console.log("contentHeight 1：" + contentHeight)
+
+	box = $("#area_2")[0]
+	contentHeight += parseFloat(window.getComputedStyle(box).height.replace("px", ""));
+	//console.log("contentHeight 2：" + contentHeight)
+
+	box = $("#area_3")[0]
+	contentHeight += parseFloat(window.getComputedStyle(box).height.replace("px", ""));
+	//console.log("contentHeight 3：" + contentHeight)
+
+	box = $("#area_4")[0]
+	contentHeight += parseFloat(window.getComputedStyle(box).height.replace("px", ""));
+	//console.log("contentHeight 4：" + contentHeight)
+
+	tableHeight = winHeight - contentHeight- 60*1.2;
+
+	leftFreezeEl.height(tableHeight);
+	scrollAreaEl.height(tableHeight);
 }
 
 adjustHeight();
@@ -21,14 +44,14 @@ win.on('resize', adjustHeight);
 
 //设置iscroll
 var myScroll = new IScroll('.t_r_content', {
-    scrollX: true,
-    scrollY: true,
-    probeType: 3
+	scrollX: true,
+	scrollY: true,
+	probeType: 3
 });
 
 //阻止默认滚动
 scrollAreaEl.on('touchmove mousewheel', function(e) {
-    e.preventDefault();
+	e.preventDefault();
 });
 
 //固定上左表头的滚动
@@ -36,8 +59,8 @@ myScroll.on('scroll', updatePosition);
 myScroll.on('scrollEnd', updatePosition);
 
 function updatePosition() {
-    var a = this.y;
-    var b = this.x;
-    leftTableEl.css('transform', 'translate(0px, ' + a + 'px) translateZ(0px)');
-    rightTableEl.css('transform', 'translate(' + b + 'px, 0px) translateZ(0px)');
+	var a = this.y;
+	var b = this.x;
+	leftTableEl.css('transform', 'translate(0px, ' + a + 'px) translateZ(0px)');
+	rightTableEl.css('transform', 'translate(' + b + 'px, 0px) translateZ(0px)');
 }
